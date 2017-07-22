db0c8c9b168076028d0efe27fa7edfd0056da5c8
Jake Lawrence <jake.lawrence24@gmail.com>
2017-07-22 16:34:03 -0400

    more frontend added, satring page changes

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,8 @@
 from flask import (abort, g, jsonify, render_template, redirect,
                    request, send_file, session, url_for)
 from manage import app, manager
+import os
+from random import randint
 from . import main
 
 @main.route('/')
