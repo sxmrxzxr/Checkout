ad16de01a20f43077c14b45080394cf63cd277ab
Jake Lawrence <jake.lawrence24@gmail.com>
2017-08-02 23:17:54 -0400

    frontend layout done, for now

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,13 +1,34 @@
 $(document).ready(function() {
     $('#inventory').DataTable({
-        "scrollY": "650px",
+        "paging": false,
+        "autoWidth": false,
+        "scrollY": "75%",
         "scrollCollapse": true,
+        "columns": [
+            { "width": "20%"},
+            { "width": "5%"},
+            { "width": "15%"},
+            { "width": "20%"},
+            { "width": "20%"},
+            { "width": "20%"}
+          ],
+        "order": [[ 1, "desc" ], [ 0, "asc" ]]
+    });
+} );
+
+$(document).ready(function() {
+    $('#hackathons').DataTable({
         "paging": false,
         "autoWidth": false,
         "columns": [
             { "width": "40%"},
             { "width": "20%"},
             { "width": "40%"}
-          ]
+          ],
+        "order": [[ 2, "desc" ]],
+        "scrollY": "150px",
+        "scrollCollapse": true,
+        bFilter: false,
+        bInfo: false
     });
 } );
